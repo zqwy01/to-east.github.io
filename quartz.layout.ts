4395cb7463530c7b7c654bb124bcb2ae6acf4886
zqwy <120390045+zqwy01@users.noreply.github.com>
2026-01-03 02:28:06 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -69,19 +69,4 @@ export const defaultListPageLayout: PageLayout = {
   right: [],
 }
 
-//
 
-Component.Explorer({
-  title: "Explorer", // title of the explorer component
-  folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
-  folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-  useSavedState: false, // whether to use local storage to save "state" (which folders are opened) of explorer
-  // omitted but shown later
-  sortFn: ...,
-  filterFn: ...,
-  mapFn: (node) => {
-    node.displayName = node.displayName.toUpperCase()
-    return node
-  },
-  order: ["filter", "map", "sort"],
-})

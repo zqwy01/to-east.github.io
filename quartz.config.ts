c9dd601cb331febcecfdb29fa33a4994b6366ce3
zqwy01 <lolos228322@yahoo.com>
2026-05-13 22:43:36 +0300

    everyday_fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -94,7 +94,7 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
-      Plugin.ClickableImages(),
+      // Plugin.ClickableImages(),
     ],
   },
 }
